e1139bf1a46d3114ba5dc1b31baa183ce7b71d94
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-18 22:06:12 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,12 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-    get'/' do 
+    get'/' do
 
-      erb :super_hero 
-    end 
+      erb :super_hero
+    end
 
-    post '/' do 
+    post '/' do
       @team = params[:team]
       @members = params[:members]
 
